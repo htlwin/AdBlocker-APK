66ea61710c064fb139b7c9dfca641e4df13b3609
htlwin <htetthulwin09@gmail.com>
2026-03-27 05:59:53 +0000

    Fix TextView import

--- app/src/main/java/com/adblocker/app/MainActivity.java
+++ app/src/main/java/com/adblocker/app/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.TextView;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
